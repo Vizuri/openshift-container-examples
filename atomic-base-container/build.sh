5aa6626ea40bb1d8764b29aea1e8c97a1d9bd850
Kent Eudy <keudy@vizuri.com>
2017-08-29 14:58:22 -0400

    initial update' '

---
+++ atomic-base-container/build.sh
@@ -0,0 +1,2 @@
+docker build --no-cache -t marriott/atomic-base-container:1.0 .
+#docker build -t marriott/atomic-base-container:1.0 .
---
+++ rhel-base-container/build.sh
@@ -0,0 +1,2 @@
+docker build --no-cache -t marriott/rhel-base-container:1.0 .
+#docker build -t marriott/rhel-base-container:1.0 .
